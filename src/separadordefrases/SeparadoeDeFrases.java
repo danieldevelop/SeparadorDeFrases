236f4b559952cf05bc59cef6e0e555826029a518
Daniel Gomez <dgomez.informatica@gmail.com>
2023-03-18 23:48:15 -0300

    tarea completada y compilada sin problemas

--- src/separadordefrases/SeparadoeDeFrases.java
+++ src/separadordefrases/SeparadoeDeFrases.java
@@ -1,7 +1,21 @@
 package separadordefrases;
 
-public class SeparadoeDeFrases {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class SeparadoeDeFrases {
+    static Scanner cin = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String frase = "";
+
+        System.out.println("Ingrese una frase inspiradora: ");
+        frase = cin.nextLine();
+
+        String[] palabras = frase.split(" ");
+
+//        for (int i = 0; i <= palabras.length - 1; i++) { // le agregamos el -1 si igualamos con un <=
+        for (int i = 0; i < palabras.length; i++) { // no le agregamos el -1 ya que el ultimo indice no lo cuenta
+            System.out.println("Palabra " + (i+1) + ": " + palabras[i]);
+        }
     }
 }
